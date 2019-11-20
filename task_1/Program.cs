9f9bc7d24c796e3426c9412482bbc53b881cb998
Ivan <ivaneremov@icloud.com>
2019-11-20 13:06:30 +0300

    fix

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace task_1
+namespace task1
 {
+
     class Program
     {
-        static void Main(string[] args)
+        enum day
         {
+
+            q,
+            w,
+            e,
+            r,
+            t
+        }
+        /*Создать список из 5 элементов и вывести все на консоль
+         * Создать массив из 5 элементов и вывести все на консоль
+         * Создать перечесление и вывести на консоль 
+         */
+
+
+        static void Main()
+        {
+            List<int> ts = new List<int>();
+            ts.Add(0);
+            ts.Add(2);
+            ts.Add(3);
+            ts.Add(4);
+            ts.Add(5);
+
+            for (int i = 0; i < ts.Count; i++)
+            {
+                Console.WriteLine($"{ts[i]}");
+
+            }
+
+            int[] mass = new int[5] { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.Write($"{mass[i]} ");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{(day)(i) }");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
